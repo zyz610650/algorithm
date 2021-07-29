53c3aa71bf8a907364f8a9f568612ad6bdf55715
zyz <zhangyuze724@163.com>
2021-07-29 21:43:59 +0800

    T44-T46

---
+++ src/zyz/algorithm/array/medium/Permutations_46.java
@@ -0,0 +1,84 @@
+package zyz.algorithm.array.medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author zyz
+ * @title: 全排列
+ * @seq: 46
+ * @address: https://leetcode-cn.com/problems/permutations/
+ * @idea:
+ */
+public class Permutations_46 {
+
+
+    static List<List<Integer>> lists=new ArrayList<>();
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,3,4};
+        System.out.println(permute(nums));
+    }
+    public static List<List<Integer>> permute(int[] nums) {
+        boolean[] vis=new boolean[nums.length];
+        List<Integer> list=new ArrayList<>();
+        dfs1(nums,vis,0,nums.length,list);
+        return lists;
+    }
+    static void  dfs(int[] nums,boolean[] vis,int index,int n,List<Integer> list)
+    {
+
+        if(index==n)
+        {
+            lists.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]) continue;
+//            if(vis[i]||(i!=0&&!vis[i-1]&&nums[i]==nums[i-1])) continue;
+            int tmp=i-1;
+            int flag=0;
+            while(tmp>=0&&nums[i]==nums[tmp])
+            {
+                if (!vis[tmp])
+                {
+                    flag=1;
+                    break;
+                }
+                tmp--;
+            }
+            if (flag==1) continue;
+            vis[i]=true;
+            list.add(nums[i]);
+            dfs(nums,vis,index+1,n,list);
+            list.remove(index);
+            vis[i]=false;
+        }
+    }
+    static void  dfs1(int[] nums,boolean[] vis,int index,int n,List<Integer> list)
+    {
+
+        if(index==n)
+        {
+            lists.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<n;i++)
+        {
+
+           if(vis[i]||(i!=0&&!vis[i-1]&&nums[i]==nums[i-1])) continue;
+            vis[i]=true;
+            list.add(nums[i]);
+            dfs(nums,vis,index+1,n,list);
+            list.remove(index);
+            vis[i]=false;
+        }
+    }
+}
+//1,1,1,2,3,4
+//
+//1(1) 4 1
+//[1, 2, 1, 1, 3, 4], [1, 2, 1, 1, 4, 3], [1, 2, 1, 3, 1, 4], [1, 2, 1, 3, 4, 1], [1, 2, 1, 4, 1, 3], [1, 2, 1, 4, 3, 1]
