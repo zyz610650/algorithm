a6283bfd8a840771fd8680d8d37029ae8e946e14
zyz <zhangyuze724@163.com>
2021-09-16 21:46:38 +0800

    t104

--- src/zyz/algorithm/CharacterString/SlidingWindow/MinimumWindowSubstring_104.java
+++ src/zyz/algorithm/CharacterString/SlidingWindow/MinimumWindowSubstring_104.java
@@ -1,9 +1,6 @@
 package zyz.algorithm.CharacterString.SlidingWindow;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author zyz
@@ -15,28 +12,39 @@ import java.util.Map;
 public class MinimumWindowSubstring_104 {
 
     public static void main(String[] args) {
+        System.out.println((int)'a');
+        System.out.println((char)95);
+//     for(int i=0;i<26;i++)
+//         System.out.println((char)('A'+i));
         String s = "ADOBECODEBANC", t = "ABC";
-        System.out.println(minWindow(s,t));
-    }
-    public static String minWindow(String s, String t) {
-        if(s.length()<t.length()) return null;
-        if(s.length()==t.length()&&!s.equals(t)) return null;
 
-        Map<Character, List<Integer>> map=new HashMap<>();
+        System.out.println(minWindow(s,t));
+
+    }
+    // 单词重复问题没有解决 比如"aa" "aa"
+    public static String minWindow1(String s, String t) {
+
+        if(s.length()<t.length()) return "";
+
+        Map<Character, Integer> map=new HashMap<>();
+        Map<Character, Integer> countMap=new HashMap<>();
         for(char ch:t.toCharArray())
-            map.put(ch,new ArrayList<>());
-        List<Integer> list=null;
+        {
+            map.put(ch,0);
+            countMap.put(ch,countMap.getOrDefault(ch,0)+1);
+        }
+
         int times=t.length();
         char[] chs=s.toCharArray();
         int ans=s.length(),ansMin=s.length(),ansMax=-1;
         int min,max;
+        int tmp=0;
         for(int i=0;i<chs.length;i++)
         {
             if(map.containsKey(chs[i]))
             {
-                list=map.get(chs[i]);
-                list.add(i);
-                map.put(chs[i],list);
+                tmp=map.get(chs[i]);
+                map.put(chs[i],i);
                 times--;
                 if(times<=0)
                 {
@@ -44,12 +52,11 @@ public class MinimumWindowSubstring_104 {
                     max=-1;
                     for(char ch:t.toCharArray())
                     {
-                        list=map.get(ch);
-                        int tmp=list.get(list.size()-1);
+                        tmp=map.get(ch);
                         min=Math.min(tmp,min);
                         max=Math.max(tmp,max);
                     }
-                    if(min==max) continue;
+//                    if(min==max) continue;
                     if(max-min<ans)
                     {
                         ans=max-min;
@@ -59,8 +66,74 @@ public class MinimumWindowSubstring_104 {
                 }
             }
         }
-
-        if (ansMax==ansMin) return null;
+        if (ansMin==s.length()) return "";
         return s.substring(ansMin,ansMax+1);
     }
+
+
+//
+    public static String minWindow3(String s, String t)
+    {
+        Map<Character,Integer> hs=new HashMap<>();
+        Map<Character,Integer> ht=new HashMap<>();
+        for (Character ch:t.toCharArray())
+        {
+            ht.put(ch,ht.getOrDefault(ch,0)+1);
+        }
+
+        String ans="";
+        int len=s.length()+1,cnt=0;
+        char ch;
+        for (int i=0,j=0;i<s.length();i++)
+        {
+            ch=s.charAt(i);
+            hs.put(ch,hs.getOrDefault(ch,0)+1);
+            if (ht.containsKey(ch)&&hs.get(ch)<=ht.get(ch)) cnt++;
+            ch=s.charAt(j);
+            //cnt不重置 并且对于ADOBECODEBANC 第一次找到ADOBEC后j一直在0处，直到i到了下标为10这个A的地方，j才开始移动，
+            // j收缩的时候
+            while(j<i&&(!ht.containsKey(ch)||hs.get(ch)>ht.get(ch)))
+            {
+                // ht里不含的字符，hs减不减1都行，这里是为了方便就都减了，要么还得写个if else;
+                hs.put(ch,hs.get(ch)-1);
+                j++;
+                ch=s.charAt(j);
+            }
+            if (cnt==t.length()&&len>i-j+1)
+            {
+                len=i-j+1;
+                ans=s.substring(j,i+1);
+            }
+        }
+        return ans;
+    }
+
+    public static String minWindow(String s, String t)
+    {
+        int[] hash=new int[128];
+        for (char ch:t.toCharArray())
+        {
+            hash[ch]--;
+        }
+        String res=""; int cnt=0;
+        int len=s.length()+1;
+        char ch;
+        for (int i=0,j=0;i<s.length();i++)
+        {
+             ch=s.charAt(i);
+            hash[ch]++;
+            if (hash[ch]<=0) cnt++;
+
+            while (j<i&&hash[s.charAt(j)]>0) hash[s.charAt(j++)]--;
+            if (cnt==t.length()&&len>i-j+1)
+            {
+                len=i-j+1;
+                res=s.substring(j,i+1);
+            }
+
+        }
+        return res;
+    }
+
+
 }
