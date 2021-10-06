8f1e20b48d211761797bf6b6525815f96c7f515d
zyz <zhangyuze724@163.com>
2021-10-06 22:16:38 +0800

    T127

---
+++ src/zyz/algorithm/CharacterString/hard/PalindromePartitioning2_128.java
@@ -0,0 +1,55 @@
+package zyz.algorithm.CharacterString.hard;
+
+import zyz.algorithm.utils.Utils;
+
+/**
+ * @author zyz
+ * @title: 分割回文串 II
+ * @seq: 132
+ * @address: https://leetcode-cn.com/problems/palindrome-partitioning-ii/
+ * @idea:
+ *
+ */
+public class PalindromePartitioning2_128 {
+
+    public static void main(String[] args) {
+        String s = "aabada";
+        System.out.println(minCut(s));
+    }
+    public  static int minCut(String s) {
+        int len=s.length();
+        boolean[][] dp=new boolean[len+1][len+1];
+        dp[0][0]=true;
+        dp[len][len]=true;
+        for(int i=1;i<len;i++)
+        {
+            dp[i][i]=true;
+            if(s.charAt(i)==s.charAt(i-1)) dp[i][i+1]=true;
+        }
+        for(int i=3;i<=len;i++)
+        {
+            for(int j=1;j+i-2<len;j++)
+            {
+                if(s.charAt(j-1)==s.charAt(j+i-2))
+                {
+                    dp[j][j+i-1]=dp[j+1][j+i-2];
+
+                }
+            }
+        }
+
+        Utils.print2Arr(dp);
+        System.out.println();
+        for (int i=1;i<=len;i++)
+        {
+            for (int j=i;j<=len;j++)
+            {
+                if (dp[i][j])
+                    System.out.println(s.substring(i-1,j));
+            }
+        }
+
+        return 0;
+    }
+
+}
