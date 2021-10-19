b5f54b22c96f7f2017cfac7cf0b716ae72484ea4
zyz <zhangyuze724@163.com>
2021-10-19 16:19:47 +0800

    T156-T159

---
+++ src/zyz/algorithm/tree/medium/PopulatingNextRightPointersinEachNode2_159.java
@@ -0,0 +1,40 @@
+package zyz.algorithm.tree.medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author zyz
+ * @title: 填充每个节点的下一个右侧节点指针 II
+ * @seq: 117
+ * @address: https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/
+ * @idea:
+ */
+public class PopulatingNextRightPointersinEachNode2_159 {
+
+	public Node connect(Node root) {
+		if(root==null) return root;
+		Deque<Node> queue=new LinkedList<>();
+		queue.addLast(root);
+		int size;
+		Node node;
+		Node pre=null;
+		while(!queue.isEmpty())
+		{
+			size=queue.size();
+			for(int i=0;i<size;i++)
+			{
+				node=queue.pollFirst();
+				if(pre!=null)
+					pre.next=node;
+				pre=node;
+				if(node.left!=null)
+					queue.addLast(node.left);
+				if(node.right!=null)
+					queue.addLast(node.right);
+			}
+			pre=null;
+		}
+		return root;
+	}
+}
--- src/zyz/algorithm/utils/TreeUtils.java
+++ src/zyz/algorithm/utils/TreeUtils.java
@@ -1,5 +1,10 @@
 package zyz.algorithm.utils;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+
 /**
  * @author: @zyz
  */
@@ -9,5 +14,31 @@ public class TreeUtils {
 
 	}
 
+
+//	public  static TreeNode createTree(List<String> list)
+//	{
+//		Deque < TreeNode > queue=new LinkedList <> ();
+//
+//		TreeNode root=new TreeNode (Integer.valueOf (list.get (0)));
+//		queue.addLast (root);
+//		TreeNode node=null;
+//		TreeNode tmp=null;
+//		boolean flag=true;
+//		for (int i=1;i<list.size ();i++)
+//		{
+//
+//			node=queue.pollFirst ();
+//			tmp=new TreeNode (Integer.valueOf (list.get (i)));
+//			if (flag)
+//			{
+//				node.left=tmp;
+//			}else {
+//				node.right=tmp;
+//			}
+//			flag=!flag;
+//			queue.addLast (tmp);
+//		}
+//
+//	}
 
 }
