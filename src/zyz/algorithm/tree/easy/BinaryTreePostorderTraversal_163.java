91b1f61b736a902124f92bc9fe8bc1c537c6335a
zyz <zhangyuze724@163.com>
2021-10-20 15:46:49 +0800

    T160-T165

---
+++ src/zyz/algorithm/tree/easy/BinaryTreePostorderTraversal_163.java
@@ -0,0 +1,28 @@
+package zyz.algorithm.tree.easy;
+
+import zyz.algorithm.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 后序遍历
+ * @author: @zyz
+ */
+public class BinaryTreePostorderTraversal_163 {
+	static List <Integer> list=new ArrayList <> ();
+	public List<Integer> postorderTraversal(TreeNode root) {
+		list.clear();
+		preOrder(root);
+		return list;
+	}
+
+	public static void preOrder(TreeNode node)
+	{
+		if(node==null) return;
+
+		preOrder(node.left);
+		preOrder(node.right);
+		list.add(node.val);
+	}
+}
---
+++ src/zyz/algorithm/tree/medium/BinarySearchTreeIterator_164.java
@@ -0,0 +1,44 @@
+package zyz.algorithm.tree.medium;
+
+import zyz.algorithm.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: @zyz
+ */
+public class BinarySearchTreeIterator_164 {
+
+
+}
+class BSTIterator {
+
+
+	static List <Integer> list=new ArrayList <> ();
+	static int index=0;
+	public BSTIterator(TreeNode root) {
+
+
+		inOrder(root);
+	}
+
+	public int next() {
+		return list.get(index++);
+	}
+
+	public boolean hasNext() {
+
+		if(index<list.size()) return true;
+		else return false;
+	}
+
+	static void inOrder(TreeNode node)
+	{
+		if(node==null) return;
+		inOrder(node.left);
+		list.add(node.val);
+		inOrder(node.right);
+	}
+}
+
