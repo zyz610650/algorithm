6159834e8cadcd63b7193d4a43bd57a86855ec83
zyz <zhangyuze724@163.com>
2021-11-08 19:43:08 +0800

    T199

--- src/zyz/algorithm/hot/hot100/CourseSchedule_199.java
+++ src/zyz/algorithm/hot/hot100/CourseSchedule_199.java
@@ -1,9 +1,7 @@
 package zyz.algorithm.hot.hot100;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zyz
@@ -14,27 +12,80 @@ import java.util.Set;
  */
 public class CourseSchedule_199 {
 
+	static int[] flag;
+	static List< List <Integer>> degree;
+
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
-		Set <Integer> set=new HashSet <> ();
+		flag=new int[numCourses];
+		degree=new ArrayList<>();
+		for(int i=0;i<numCourses;i++)
+		{
+			degree.add (new ArrayList <> ());
+		}
 
-		Arrays.sort (prerequisites , new Comparator < int[] > () {
-			@Override
-			public int compare (int[] o1 , int[] o2) {
-				return o1[0]-o1[0];
-			}
-	});
-
-		set.add(prerequisites[0][1]);
 		for(int i=0;i<prerequisites.length;i++)
 		{
-			if(!set.contains(prerequisites[i][1]))
-			{
-				return false;
-			}else{
-				set.add(prerequisites[i][0]);
-			}
+			degree.get(prerequisites[i][1]).add(prerequisites[i][0]);
+		}
+		// 比如 [2,3][4,5] 2,3和4,5没有必然的联系 一次dfs不能遍历完
+		// 所以这里循环
+		for(int i=0;i<numCourses;i++)
+		{
+			if(!dfs(i)) return false;
 		}
 		return true;
 
 	}
+
+	public static boolean dfs(int index)
+	{
+		if(flag[index]==1) return true;
+		if(flag[index]==-1) return false;
+		flag[index]=-1;
+		for(int i=0;i<degree.get(index).size();i++)
+		{
+			if(!dfs(degree.get(index).get(i))) return false;
+		}
+		flag[index]=1;
+		return true;
+	}
+//BFS
+
+//	static List< List <Integer>> adj;
+//	static int[] degree;
+//	public boolean canFinish(int numCourses, int[][] prerequisites) {
+//		Deque <Integer> queue=new LinkedList <> ();
+//		adj=new ArrayList<>();
+//		degree=new int[numCourses];
+//
+//		for(int i=0;i<numCourses;i++)
+//		{
+//			adj.add (new ArrayList <> ());
+//		}
+//
+//		for(int i=0;i<prerequisites.length;i++)
+//		{
+//			adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+//			degree[prerequisites[i][0]]++;
+//		}
+//
+//		for(int i=0;i<numCourses;i++)
+//		{
+//			if(degree[i]==0)
+//				queue.add(i);
+//		}
+//		int t;
+//		while(!queue.isEmpty())
+//		{
+//			t=queue.poll();
+//			numCourses--;
+//			for(int i=0;i<adj.get(t).size();i++)
+//			{
+//				if((--degree[adj.get(t).get(i)])==0) queue.add(adj.get(t).get(i));
+//			}
+//		}
+//		if(numCourses==0) return true;
+//		return false;
+//
+//	}
 }
